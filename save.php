83f8755123c81e6b84b52ec6186338796f4cefe8
blckink <86264410+blckink@users.noreply.github.com>
2025-08-22 18:22:42 +0200

    Add enemy layer and remove hidden floors

--- save.php
+++ save.php
@@ -6,11 +6,13 @@ header('Pragma: no-cache');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $input = file_get_contents('php://input');
     $data = json_decode($input, true);
-    if (isset($data['collisions']) && isset($data['gems'])) {
+    if (isset($data['collisions']) && isset($data['gems']) && isset($data['enemies'])) {
         $collisions = 'const collisions = ' . json_encode($data['collisions']) . ';';
         $gems = 'const l_Gems = ' . json_encode($data['gems']) . ';';
+        $enemies = 'const l_Enemies = ' . json_encode($data['enemies']) . ';';
         file_put_contents(__DIR__ . '/data/collisions.js', $collisions);
         file_put_contents(__DIR__ . '/data/l_Gems.js', $gems);
+        file_put_contents(__DIR__ . '/data/l_Enemies.js', $enemies);
         echo 'Saved';
     } else {
         http_response_code(400);
